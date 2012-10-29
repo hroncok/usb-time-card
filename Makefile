3e0b6d60c7b167f8a73541bbda5fe209833105c4
Miro Hrončok <miro@hroncok.cz>
2012-10-29 11:54:54 +0100

    Automated version, 0.5

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 CC=gcc
 CFLAGS=-Wall -pedantic -g
 LIBS=-ludev -lconfig
+VERSION=$(shell grep "define VERSION" usb-time-card-deamon.c | cut -f3 -d" " | cut -f2 -d\")
 
 usb-time-card-deamon: usb-time-card-deamon.c
 	$(CC) -o $@ $^ $(CFLAGS) $(LIBS)
@@ -28,7 +29,8 @@ install: root
 debian: root
 	rm root/DEBIAN -rf
 	cp -ra DEBIAN root
-	dpkg -b root usb-time-card_0.4.3_amd64.deb
+	sed -i "s/VERSION/$(VERSION)/" root/DEBIAN/control
+	dpkg -b root usb-time-card_$(VERSION)_amd64.deb
 
 clean: 
 	rm usb-time-card-deamon root *.deb -rf
--- usb-time-card-deamon.c
+++ usb-time-card-deamon.c
@@ -8,6 +8,7 @@
 #include <libconfig.h>
 #include <time.h>
 
+#define VERSION "0.5"
 #define TIME		300
 #define SERIAL		"111111111111111"
 #define LOGFILE		"/var/log/usb-time-card.log"
@@ -19,56 +20,6 @@
 /* Global variable for SIGTERM handling */
 int gcont;
 
-/* Default configuration */
-void defaultConfig(int * waittime,const char ** serial,const char ** log,const char ** html) {
-	*waittime = TIME;
-	*serial = SERIAL;
-	*log = LOGFILE;
-	*html = HTMLFILE;
-}
-
-/* In file configuration */
-void loadConfig(config_t *cf, const char * config, int * waittime,const char ** serial,const char ** log,const char ** html) {
-	FILE *logfile;
-	
-	/* Default config */
-	defaultConfig(waittime,serial,log,html);
-	
-	config_init(cf);
-	
-	/* Check config file syntax */
-	if (!config_read_file(cf, config)) {
-		fprintf(stderr, "%s:%d - %s\n",config_error_file(cf),config_error_line(cf),config_error_text(cf));
-		config_destroy(cf);
-		exit(EXIT_FAILURE);
-	}
-	
-	/* Load variables */
-	config_lookup_int(cf, "waittime", waittime);
-	config_lookup_string(cf, "serial", serial);
-	config_lookup_string(cf, "log", log);
-	config_lookup_string(cf, "html", html);
-	
-	/* Validation */
-	if (*waittime < 0) {
-		fprintf(stderr, "Found negative value in waittime variable in config file. Not going to work.\n");
-		config_destroy(cf);
-		exit(EXIT_FAILURE);
-	}
-	
-	/* Create output */
-	mkdirp(*log);
-	logfile = fopen(*log,"a");
-	if (!logfile) {
-		perror("Cannot open log file for writing");
-		exit(EXIT_FAILURE);
-	}
-	fclose(logfile);
-	exportHTML(*log,*html);
-	
-	/* Destroy config before exiting the program */
-}
-
 /* Call system("mkdir -p dirname");
    This is the most easy solution */
 void mkdirp(const char * filename) {
@@ -77,65 +28,6 @@ void mkdirp(const char * filename) {
 	system(cmd);
 }
 
-/* Determinate, if the USB disk is present */
-int USBDiskPresent(const char * serial) {
-	/* Lots of things copied from this tutorial:
-	   http://www.signal11.us/oss/udev/
-	   
-	   It says:
-	   
-	   This code is meant to be a teaching
-	   resource. It can be used for anyone for
-	   any reason, including embedding into
-	   a commercial product. */
-	struct udev *udev;
-	struct udev_enumerate *enumerate;
-	struct udev_list_entry *devices, *dev_list_entry;
-	struct udev_device *dev;
-	
-	/* Create the udev object */
-	if (!(udev = udev_new())) {
-		perror("Can't create udev");
-		exit(EXIT_FAILURE);
-	}
-	
-	/* Create a list of the devices in the 'block' subsystem */
-	enumerate = udev_enumerate_new(udev);
-	udev_enumerate_add_match_subsystem(enumerate, "block");
-	udev_enumerate_scan_devices(enumerate);
-	devices = udev_enumerate_get_list_entry(enumerate);
-	
-	/* For each item enumerated, chceck if it's our device */
-	udev_list_entry_foreach(dev_list_entry,devices) {
-		const char *path;
-		
-		/* Get the filename of the /sys entry for the device
-		   and create a udev_device object (dev) representing it */
-		path = udev_list_entry_get_name(dev_list_entry);
-		dev = udev_device_new_from_syspath(udev,path);
-
-		/* The device pointed to by dev contains information about
-		   the block device. In order to get information about the
-		   USB device, get the parent device with the
-		   subsystem/devtype pair of "usb"/"usb_device". */
-		dev = udev_device_get_parent_with_subsystem_devtype(dev,"usb","usb_device");
-		if (dev) {
-			if (! strcmp(udev_device_get_sysattr_value(dev,"serial"),serial)) {
-				/* Free the device and enumerator objects */
-				udev_device_unref(dev);
-				udev_enumerate_unref(enumerate);
-				udev_unref(udev);
-				return 1;
-			}
-			/* Free the device object anyway */
-			udev_device_unref(dev);
-		}
-	}
-	/* Free the enumerator object */
-	udev_enumerate_unref(enumerate);
-	udev_unref(udev);
-	return 0;       
-}
 
 /* Proccess the file backquards */
 void backquards(FILE * logfile, FILE * htmlfile) {
@@ -198,6 +90,116 @@ void exportHTML(const char * log, const char * html) {
 	fclose(htmlfile);
 }
 
+/* Default configuration */
+void defaultConfig(int * waittime,const char ** serial,const char ** log,const char ** html) {
+	*waittime = TIME;
+	*serial = SERIAL;
+	*log = LOGFILE;
+	*html = HTMLFILE;
+}
+
+/* In file configuration */
+void loadConfig(config_t *cf, const char * config, int * waittime,const char ** serial,const char ** log,const char ** html) {
+	FILE *logfile;
+	
+	/* Default config */
+	defaultConfig(waittime,serial,log,html);
+	
+	config_init(cf);
+	
+	/* Check config file syntax */
+	if (!config_read_file(cf, config)) {
+		fprintf(stderr, "%s:%d - %s\n",config_error_file(cf),config_error_line(cf),config_error_text(cf));
+		config_destroy(cf);
+		exit(EXIT_FAILURE);
+	}
+	
+	/* Load variables */
+	config_lookup_int(cf, "waittime", waittime);
+	config_lookup_string(cf, "serial", serial);
+	config_lookup_string(cf, "log", log);
+	config_lookup_string(cf, "html", html);
+	
+	/* Validation */
+	if (*waittime < 0) {
+		fprintf(stderr, "Found negative value in waittime variable in config file. Not going to work.\n");
+		config_destroy(cf);
+		exit(EXIT_FAILURE);
+	}
+	
+	/* Create output */
+	mkdirp(*log);
+	logfile = fopen(*log,"a");
+	if (!logfile) {
+		perror("Cannot open log file for writing");
+		exit(EXIT_FAILURE);
+	}
+	fclose(logfile);
+	exportHTML(*log,*html);
+	
+	/* Destroy config before exiting the program */
+}
+
+/* Determinate, if the USB disk is present */
+int USBDiskPresent(const char * serial) {
+	/* Lots of things copied from this tutorial:
+	   http://www.signal11.us/oss/udev/
+	   
+	   It says:
+	   
+	   This code is meant to be a teaching
+	   resource. It can be used for anyone for
+	   any reason, including embedding into
+	   a commercial product. */
+	struct udev *udev;
+	struct udev_enumerate *enumerate;
+	struct udev_list_entry *devices, *dev_list_entry;
+	struct udev_device *dev;
+	
+	/* Create the udev object */
+	if (!(udev = udev_new())) {
+		perror("Can't create udev");
+		exit(EXIT_FAILURE);
+	}
+	
+	/* Create a list of the devices in the 'block' subsystem */
+	enumerate = udev_enumerate_new(udev);
+	udev_enumerate_add_match_subsystem(enumerate, "block");
+	udev_enumerate_scan_devices(enumerate);
+	devices = udev_enumerate_get_list_entry(enumerate);
+	
+	/* For each item enumerated, chceck if it's our device */
+	udev_list_entry_foreach(dev_list_entry,devices) {
+		const char *path;
+		
+		/* Get the filename of the /sys entry for the device
+		   and create a udev_device object (dev) representing it */
+		path = udev_list_entry_get_name(dev_list_entry);
+		dev = udev_device_new_from_syspath(udev,path);
+
+		/* The device pointed to by dev contains information about
+		   the block device. In order to get information about the
+		   USB device, get the parent device with the
+		   subsystem/devtype pair of "usb"/"usb_device". */
+		dev = udev_device_get_parent_with_subsystem_devtype(dev,"usb","usb_device");
+		if (dev) {
+			if (! strcmp(udev_device_get_sysattr_value(dev,"serial"),serial)) {
+				/* Free the device and enumerator objects */
+				udev_device_unref(dev);
+				udev_enumerate_unref(enumerate);
+				udev_unref(udev);
+				return 1;
+			}
+			/* Free the device object anyway */
+			udev_device_unref(dev);
+		}
+	}
+	/* Free the enumerator object */
+	udev_enumerate_unref(enumerate);
+	udev_unref(udev);
+	return 0;       
+}
+
 /* Write the status to log */
 void writeStatus(const char * serial, int present, const char * log, const char * html) {
 	const char *way;
@@ -243,6 +245,18 @@ int main(int argc, char **argv) {
 	int present, waittime;
 	const char *serial, *log, *html;
 	config_t cfg;
+	
+	/* --help and --version */
+	if (argc == 2) {
+		if (!strcmp(argv[1],"--help")) {
+			printf("For help information, run man usb-time-card\n");
+			return EXIT_SUCCESS;
+		}
+		if (!strcmp(argv[1],"--version")) {
+			printf("usb-time-card %s\n",VERSION);
+			return EXIT_SUCCESS;
+		}
+	}
 	
 	/* Config file */
 	if ((argc != 3) || strcmp(argv[1],"--config")) {
