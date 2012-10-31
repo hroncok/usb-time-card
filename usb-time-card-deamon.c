be26269f942dce7b166e83376cae4e02117c819c
Miro Hrončok <miro@hroncok.cz>
2012-10-31 05:20:03 +0100

    Version 0.5.1

--- usb-time-card-deamon.c
+++ usb-time-card-deamon.c
@@ -8,7 +8,7 @@
 #include <libconfig.h>
 #include <time.h>
 
-#define VERSION 	"0.5"
+#define VERSION 	"0.5.1"
 #define TIME		300
 #define SERIAL		"111111111111111"
 #define LOGFILE		"/var/log/usb-time-card.log"
